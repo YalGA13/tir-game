76072c1923dc3fb9744b2cdb2370a5c3c10ef4c3
GAYAGood <yalgen5@gmail.com>
2025-05-10 18:35:18 +0200

    add new target

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/icon.164431.jpg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/photo_2.jpg")
+target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
