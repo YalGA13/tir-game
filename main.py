d65f919eabb7504acf72e60825c388d5c3a49944
GAYAGood <yalgen5@gmail.com>
2025-05-10 17:50:37 +0200

    add cons code

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame
 from pygame.examples.go_over_there import running
+import random
 
 pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("img/icon.photo_2025-05-10_16-44-31.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/photo_2.jpg")
+target_width = 80
+target_height = 80
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
